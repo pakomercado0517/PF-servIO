c8e574110e5c3f77db730e09d5c72c2bcd5bbe04
imanolcorimayo <imanolcorimayo@gmail.com>
2021-12-08 14:53:47 -0300

    reestructuracion

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import NavBar from '../components/NavBar';
+// import NavBar from '../components/NavBar';
 import Landing from '../components/Landing';
 
 export default function Home(){
     return (
         <div>
-            <NavBar/>
+            {/* <NavBar/> */}
             <Landing></Landing>
         </div>    
     )
