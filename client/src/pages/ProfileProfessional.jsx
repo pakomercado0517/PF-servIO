4da629e94670667ce2c4375fbb0965184713d7ef
imanolcorimayo <imanolcorimayo@gmail.com>
2021-12-08 16:33:34 -0300

    Profile Professional

--- client/src/pages/ProfileProfessional.jsx
+++ client/src/pages/ProfileProfessional.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import s from './styles/ProfileProfessional.module.css'
+
+import star from '../img/star.svg'
+import { FaRegEdit } from 'react-icons/fa'
+
 export default function ProfileProfessional(){
     return (
-        <div>
-        
+        <div className={ s.container }>
+            <Link to="/">
+                <button className={ s.hardcode }>
+                    volviendoo
+                </button>
+            </Link>
+            <div className={ s.details }>
+                <div className={ s.photo }>
+                </div>
+                <div className={ s.text }>
+                    <h1>Elon Musk</h1>
+                    <h2>Mecánica automotriz.</h2>
+                    <h5>Buenos Aires, Argentina.</h5>
+                    <div>
+                        <img src={ star } alt="" />
+                        <img src={ star } alt="" />
+                        <img src={ star } alt="" />
+                        <img src={ star } alt="" />
+                        <img src={ star } alt="" />
+                    </div>
+                    
+                    <Link className={ s.linkEdit } to="/">
+                            <FaRegEdit size="40px" className={ s.logoEdit }>
+                            </FaRegEdit>
+                        
+                    </Link>
+                </div>
+            </div>
         </div>    
     )
 }
