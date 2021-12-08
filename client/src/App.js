9146993b4c1f91fb988aad1fb16873a9663f5abb
anamad8 <stellam_anabel@hotmail.com>
2021-12-08 00:02:05 -0300

    landg-nav-roter

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/home/Home';
+import ProfileClient from './pages/profile-client/ProfileClient';
+import ProfileProfessional from './pages/profile-professional/ProfileProfessional';
+
+function App() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/:idClient" element={<ProfileClient />}/>
+      <Route exact path="/:idProfessional" element={<ProfileProfessional/>}/>
+      {/* <Route exact path="/" element={<Home />} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/" element={<Home />} /> */}
+      
+    </Routes>
+  );
+}
+
+export default App;
