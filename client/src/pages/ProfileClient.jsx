ad309cad33f5d5396f0fbe952bc1f39fff41d93f
anamad8 <stellam_anabel@hotmail.com>
2021-12-09 13:03:10 -0300

    pagina de cliente

--- client/src/pages/ProfileClient.jsx
+++ client/src/pages/ProfileClient.jsx
@@ -5,6 +5,7 @@ import { BsArrowRightCircle } from 'react-icons/bs'
 import s from './styles/ProfileClient.module.css'
 import CardReview from '../components/CardReview';
 import CardParticularService from '../components/CardParticularService';
+import imgCliente from '../img/user_profile_photo.jpg'
 
 export default function ProfileClient(){
 
@@ -12,36 +13,46 @@ export default function ProfileClient(){
     return (
         <div className={ s.div_principal }>
             <div className={ s.div_inicio  }></div>
-            <div className={ s.div_photo  }> </div> 
+            <div className={ s.div_photo  }><img src={imgCliente } alt="" /></div>
             <div className={ s.div_info  }>
-                <h1>Lana Roadhes</h1>
-                <h2>@lali</h2>
-                <h1>Buenos Aires, Argentina</h1>
-                <h5>Telefono: +54 9 11-2233-4455</h5>
-                <h5>Email: LaliRhoades@gmail.com</h5>
-                <h2>Ultimas solicitudes:</h2>
-                <hr/>
-            </div>
-            <Link className={ s.linkEdit } to="/">
+                <div className={ s.div_tiulo}>
+                    <h3>Lana Roadhes</h3>
+                    <Link className={ s.linkEdit } to="/">
                         <FaRegEdit size="40px" className={ s.logoEdit }>
                         </FaRegEdit>
-                </Link>
+                    </Link>
+                </div>
+                <p>@lali</p>
+                <p>Buenos Aires, Argentina</p>
+                <p>Telefono:<span>+54 9 11-2233-4455</span> </p>
+                <p>Email: <span>LaliRhoades@gmail.com</span></p>
+                <p>Ultimas solicitudes:</p>
+                <div className={ s.reviews }>
+                    <h4>Ultimas Solicitudes</h4>
+                    <CardParticularService/>
+                    <CardParticularService/>
+                    <CardParticularService/>
+                    <CardParticularService/>
+                    <CardParticularService/>
+                    <CardParticularService/>
+                    <CardParticularService/>
+                    <CardParticularService/>
+                    <BsArrowRightCircle className={s.reviews_icon} size="50px"/>
+                    <span >Ver Historial</span> 
+                </div>
+                <div className={ s.reviews }>
+                    <h4>Ultimas Reseñas</h4>
+                    <CardReview/>
+                    <CardReview/>
+                    <CardReview/>
+                    <CardReview/>
+                    <BsArrowRightCircle className={s.reviews_icon} size="50px"/>
+                    <span >Ver Historial</span> 
+                </div>
+            </div>
+            
+            
 
-                <h6>Ultimas Solicitudes</h6>
-            <div className={ s.reviews }>
-                <CardParticularService/>
-                <CardParticularService/>
-                <CardParticularService/>
-                <BsArrowRightCircle size="50px"/>
-                <>ver historial</>
-            </div>
-                <h6>Ultimas Reseñas</h6>
-            <div className={ s.reviews }>
-                <CardReview/>
-                <CardReview/>
-                <CardReview/>
-                <BsArrowRightCircle size="50px"/>
-            </div>
         </div>
     )
 }
