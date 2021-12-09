1127d0e487695cc5907041957b21cb2fb26805cf
imanolcorimayo <imanolcorimayo@gmail.com>
2021-12-08 22:01:34 -0300

    NavBar implementation

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import s from './NavBar.module.css';
-import logo from '../../img/logo.png';
+import s from './styles/NavBar.module.css'
+import logo from '../img/logo.png';
 // import icon from '../../img/iconHamburguesa.png';
 import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
     return (
-        <div className={s.conteiner}>
-            <div className={s.conteiner__img}>
-                <img src={logo} alt="Logo" />
+        <div className={ s.conteiner }>
+            <div className={ s.conteiner__img }>
+                <img src={ logo } alt="Logo" />
             </div>
             <div className={s.conteiner__input}>
                 <input className={s['conteiner__input--text']}
-                    type= 'text'
+                    type='text'
                     placeholder="Busca Tecnico">
                 </input>
                 <div className={s.conteiner__btn}>
-                    <button type= 'submit'className={s['conteiner__btn--btn']}></button>
+                    <button type='submit' className={s['conteiner__btn--btn']}></button>
                 </div>
             </div>
             <div className={s.conteiner__inicio}>
@@ -24,17 +24,17 @@ export default function NavBar() {
                 <NavLink to='/:idProfessional' className={s['conteiner__inicio--btn']}>Sobre Nosotros</NavLink>
             </div>
             <div className={s.conteiner__Hamb}>
-                    <div class={s['conteiner__Hamb--menu']}>
-                        <button to='/:idProfessional' className={s['conteiner__Hamb--btn']}>X</button>
-                            <div className={s['conteiner__Hamb--down']}>
-                                <div className={s['conteiner__Hamb--table']}>
-                                    {/* <img src={logo} alt="Logo" height='60' /> */}
-                                    <a>Servicios</a>
-                                    <a>Trabajos</a>
-                                    <a>Ajustes</a>
-                                </div>
-                            </div>
+                <div class={s['conteiner__Hamb--menu']}>
+                    <button to='/:idProfessional' className={s['conteiner__Hamb--btn']}>X</button>
+                    <div className={s['conteiner__Hamb--down']}>
+                        <div className={s['conteiner__Hamb--table']}>
+                            <img src={logo} alt="Logo" height='60' />
+                            <span>Servicios</span>
+                            <span>Trabajos</span>
+                            <span>Ajustes</span>
                         </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import s from './styles/Home.module.css'
-// import NavBar from '../components/NavBar';
+import NavBar from '../components/NavBar';
 import Landing from '../components/Landing';
 
 export default function Home(){
     return (
         <div>
-            {/* <NavBar/> */}
+            <NavBar/>
             <Landing></Landing>
             <Link  to="/profesional">
                 <button className={ s.hardcode }>
