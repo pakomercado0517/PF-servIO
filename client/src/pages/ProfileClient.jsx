b37ebae33f1ace83923b676bbb344d952011b8aa
ferdeolazabal <ferdeolazabal@gmail.com>
2021-12-08 21:05:14 -0300

    client profile

--- client/src/pages/ProfileClient.jsx
+++ client/src/pages/ProfileClient.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-// @ts-ignore
+import { Link } from 'react-router-dom';
+import { FaRegEdit } from 'react-icons/fa'
+import { BsArrowRightCircle } from 'react-icons/bs'
 import s from './styles/ProfileClient.module.css'
+import CardReview from '../components/CardReview';
+import CardParticularService from '../components/CardParticularService';
 
 export default function ProfileClient(){
 
@@ -8,14 +12,36 @@ export default function ProfileClient(){
     return (
         <div className={ s.div_principal }>
             <div className={ s.div_inicio  }></div>
-            <div className={ s.div_photo  }> Lana Roadhes</div>
-            <div className={ s.div_info  }>@lali</div>
-            <div className={ s.div_info2  }>Buenos Aires, Argentina</div>
-            <div className={ s.div_info3  }>Telefono: +54 9 11-2233-4455</div>
-            <div className={ s.div_info4  }>Email: LaliRhoades@gmail.com</div>
-            <div className={ s.div_info5  }>Ultimas solicitudes:</div>
+            <div className={ s.div_photo  }> </div> 
+            <div className={ s.div_info  }>
+                <h1>Lana Roadhes</h1>
+                <h2>@lali</h2>
+                <h1>Buenos Aires, Argentina</h1>
+                <h5>Telefono: +54 9 11-2233-4455</h5>
+                <h5>Email: LaliRhoades@gmail.com</h5>
+                <h2>Ultimas solicitudes:</h2>
+                <hr/>
+            </div>
+            <Link className={ s.linkEdit } to="/">
+                        <FaRegEdit size="40px" className={ s.logoEdit }>
+                        </FaRegEdit>
+                </Link>
 
-
-        </div>    
+                <h6>Ultimas Solicitudes</h6>
+            <div className={ s.reviews }>
+                <CardParticularService/>
+                <CardParticularService/>
+                <CardParticularService/>
+                <BsArrowRightCircle size="50px"/>
+                <>ver historial</>
+            </div>
+                <h6>Ultimas Reseñas</h6>
+            <div className={ s.reviews }>
+                <CardReview/>
+                <CardReview/>
+                <CardReview/>
+                <BsArrowRightCircle size="50px"/>
+            </div>
+        </div>
     )
 }
