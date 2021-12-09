b780e92540ab58cda0f30196cdebf4de89c12f3d
mariorocha2308 <virgoroch852@gmail.com>
2021-12-09 10:44:17 -0600

    add(styles&refactor)

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,40 +1,45 @@
 import React from 'react';
 import s from './styles/NavBar.module.css'
-import logo from '../img/logo.png';
-// import icon from '../../img/iconHamburguesa.png';
+import logo from '../img/ServIO.svg';
 import { NavLink } from 'react-router-dom';
+import {FiSearch} from 'react-icons/fi'
 
 export default function NavBar() {
     return (
-        <div className={ s.conteiner }>
-            <div className={ s.conteiner__img }>
+        <div className={ s.navbar }>
+            <div className={ s.container__logo }>
                 <img src={ logo } alt="Logo" />
             </div>
-            <div className={s.conteiner__input}>
-                <input className={s['conteiner__input--text']}
-                    type='text'
-                    placeholder="Busca Tecnico">
-                </input>
-                <div className={s.conteiner__btn}>
-                    <button type='submit' className={s['conteiner__btn--btn']}></button>
+            <div className={s.container__elements}>
+                <div className={s.container__input}>
+                    <FiSearch/>
+                    <input 
+                        className={s['container__input--text']}
+                        type='text'
+                        placeholder="Buscar Tecnico">
+                    </input>
+                        {/* <div className={s.conteiner__btn}>
+                        <button type='submit' className={s['conteiner__btn--btn']}></   button>
+                        </div> */}
                 </div>
-            </div>
-            <div className={s.conteiner__inicio}>
-                <NavLink to='/:idClient' className={s['conteiner__inicio--btn']}>Inicio</NavLink>
-                <NavLink to='/:idProfessional' className={s['conteiner__inicio--btn']}>Sobre Nosotros</NavLink>
-            </div>
-            <div className={s.conteiner__Hamb}>
-                <div class={s['conteiner__Hamb--menu']}>
-                    <button to='/:idProfessional' className={s['conteiner__Hamb--btn']}>X</button>
-                    <div className={s['conteiner__Hamb--down']}>
-                        <div className={s['conteiner__Hamb--table']}>
-                            <img src={logo} alt="Logo" height='60' />
-                            <span>Servicios</span>
-                            <span>Trabajos</span>
-                            <span>Ajustes</span>
+
+                <div className={s.container__navigate}>
+                    <NavLink to='/:idClient' className={s['container__inicio--btn'] }   >Inicio</NavLink>
+                    <NavLink to='/:idProfessional' className={s['container__inicio--btn']}>Sobre Nosotros</NavLink>
+                </div>
+                {/*
+                <div className={s.conteiner__Hamb}>
+                    <div class={s['conteiner__Hamb--menu']}>
+                        <button to='/:idProfessional' className={s['conteiner__Hamb--btn']} >X</button>
+                        <div className={s['conteiner__Hamb--down']}>
+                            <div className={s['conteiner__Hamb--table']}>
+                                <span>Servicios</span>
+                                <span>Trabajos</span>
+                                <span>Ajustes</span>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
