22e40f594439951a3fe1d46512d0d60c4b38b813
ferdeolazabal <ferdeolazabal@gmail.com>
2021-12-10 09:46:00 -0300

    login added

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,21 @@ import Home from './pages/Home';
 import ProfileClient from './pages/ProfileClient';
 import ProfileProfessional from './pages/ProfileProfessional';
 import Registration from './pages/Registration';
+import Login  from './components/Login';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
-      <Route exact path="/idClient" element={<ProfileClient />} />
-      <Route exact path="/:idProfessional" element={<ProfileProfessional />} />
+      <Route exact path="/clients/idClient" element={<ProfileClient />} />
+      {/* <Route exact path="/professionals/:idProfessional" element={<ProfileProfessional />} /> */}
       <Route exact path="/profesional" element={<ProfileProfessional />} />
       <Route exact path="/registration" element={<Registration />} />
       {/* <Route exact path="/" element={<Home />} />
       <Route exact path="/" element={<Home />} />
       <Route exact path="/" element={<Home />} /> */}
+
+      <Route exact path="/login" element={<Login />} />
       
     </Routes>
   );
