44ada03aa99c4309987e68222f820114162b3552
Guillermo Villagomez <gvdelamora@gmail.com>
2021-12-12 11:36:19 -0600

    update

--- api/src/routes/login.mjs
+++
@@ -1,8 +0,0 @@
-import loginFunctions from "../controllers/login.mjs";
-import { Router } from "express";
-
-const router = Router();
-
-router.post("/login", loginFunctions.login);
-
-export default router;
