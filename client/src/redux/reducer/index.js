0f40576de67069058f90aa815c1e08f81130612e
mariorocha2308 <virgoroch852@gmail.com>
2021-12-12 15:33:50 -0600

    add(filterlogic)

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,4 +1,4 @@
-import {GET_ALL_NEEDS, GET_ALL_PROFESSIONALS, GET_BY_USER_ID, NEW_USER, SEARCH_PROFESSIONAL_BY_NAME} from '../actions'
+import {GET_ALL_NEEDS, GET_ALL_PROFESSIONALS, GET_BY_USER_ID, NEW_USER, SEARCH_PROFESSIONAL_BY_NAME, ORDER_DATA} from '../actions'
 
 const initialState = {
     professionals: [],
@@ -33,6 +33,30 @@ function rootReducer( state = initialState, { type, payload } ) {
             return {
                 ...state,
                 professionals: payload
+            }
+        case ORDER_DATA:
+            let option = [];
+            switch (payload) {
+                case 'Z-A':
+                    option = state.professionals.sort((a,b) => {
+                        if (a.first_name > b.first_name) return 1;
+                        if(a.first_name < b.first_name) return -1;
+                        return 0;
+                    })
+                    break;
+                case 'A-Z':
+                    option = state.professionals.sort((a,b) => {
+                        if (a.first_name > b.first_name) return -1;
+                        if(a.first_name < b.first_name)return 1;
+                        return 0;
+                    })
+                    break
+                default:
+                    break;
+            }
+            return {
+                ...state,
+                professionals: option
             }
         default:
             return state;
