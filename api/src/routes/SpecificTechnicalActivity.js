0c789cf3e839ad3dd5e13c9dc1d8beb13d45dd35
Guillermo Villagomez <gvdelamora@gmail.com>
2021-12-21 19:55:16 -0600

    get ClientNeed by id

--- api/src/routes/SpecificTechnicalActivity.js
+++ api/src/routes/SpecificTechnicalActivity.js
@@ -3,6 +3,7 @@ const router = Router();
 const userFunctions = require('../controllers/index.js')
 
 router.get('/UserByActivityName', userFunctions.getUserByActivityName )
+
 router.get('/all', userFunctions.getAllActivities )
 router.get('/ActivityByActivityName', userFunctions.getByActivityName )
 router.post('/', userFunctions.newTechnicalActivity)
