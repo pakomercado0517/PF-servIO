a5138e7d96c0829a7aeb77d78a4c3b4863a118a1
imanolcorimayo <imanolcorimayo@gmail.com>
2021-12-22 00:05:17 -0300

    add detailsClientNeed

--- client/src/components/DetailsClientNeed.jsx
+++ client/src/components/DetailsClientNeed.jsx
@@ -5,17 +5,22 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faPhoneAlt} from '@fortawesome/free-solid-svg-icons'
 import {StarRating} from './StarRating'
 import s from './styles/DetailsClientNeed.module.css'
-import { getByUserId } from '../redux/actions'
+import { getByUserId, getDetailsClientNeed } from '../redux/actions'
 
 export default function DetailsClientNeed() {
-  const state = useSelector(state => state)
+  const { detailsClientNeed, user } = useSelector(state => state)
   const { id } = useParams()
   const dispatch = useDispatch()
-  const user = useSelector(state => state.user)
   let ranked= 2.6
   useEffect(()=> {
-    dispatch(getByUserId(id))
+    dispatch(getDetailsClientNeed(id))
   }, [])
+  useEffect(()=> {
+    dispatch(getByUserId(detailsClientNeed.UserId))
+  }, [detailsClientNeed])
+  useEffect(() => {
+      console.log(detailsClientNeed)
+  }, [user])
   return (
     <div>
       <div className={s.container_ativity}>
@@ -25,19 +30,19 @@ export default function DetailsClientNeed() {
           {/* |---------------------------Profile Photo...----------------------------------------| */}
 
           <div className={s.professional_img}>
-            <img src={"https://i.pinimg.com/236x/b3/bc/1b/b3bc1baf429da3d6509431ea26768d39.jpg"} className={s.p_image} />
+            <img src={ user[0]?.photo } className={s.p_image} />
           </div>
 
           {/* |---------------------------Float card...----------------------------------------| */}
 
           <div className={s.professional_floatCard}>
               <div className={s.floatCard_title}>
-                <h3>{"Imanol Corimayo"}</h3>
+                <h3>{ user[0]?.first_name + " " + user[0]?.last_name}</h3>
               </div>
               <div className={s.floatCard_body}>
-                <p className={s.floatCard_p}>{`@Imanol`}</p>
+                <p className={s.floatCard_p}>{`@${user[0]?.first_name}`}</p>
                 <p className={s.floatCard_p}><StarRating stars={ranked}/> <span className={s.floatCard_span}>{`${ranked}`}</span></p>
-                <p className={s.floatCard_p}><FontAwesomeIcon icon={faPhoneAlt}/><strong>Teléfono: </strong><span className={s.floatCard_span}>{"3513467739"}</span></p>
+                <p className={s.floatCard_p}><FontAwesomeIcon icon={faPhoneAlt}/><strong>Teléfono: </strong><span className={s.floatCard_span}>{user[0]?.phone}</span></p>
               </div>
           </div>
 
@@ -47,11 +52,11 @@ export default function DetailsClientNeed() {
             <div><h3 className={s.professions_title}>Profesiones:</h3></div>
             <div className={s.professions_container}>
               {
-              user[0]?.Professional.Professions.map(el=> {
-                return(
-                  <div className={s.profession}>{"Hola"}</div>
-                )
-                })
+            //   user[0]?.Professional.Professions.map(el=> {
+            //     return(
+            //       <div className={s.profession}>{"Hola"}</div>
+            //     )
+            //     })
               }
             </div>
           </div>
@@ -59,11 +64,11 @@ export default function DetailsClientNeed() {
         {/* |-------------------------------body--------------------------------------| */}
 
         <div className={s.activity_body}>
-            <h2 className={s.body_title}>holaa</h2>
+            <h2 className={s.body_title}>{ detailsClientNeed?.name }</h2>
             <div className={s.description_card}>
               <h3>Descripción</h3>
               <div className={s.description_text}>
-                <p>{"professionalActivities[0]?.description"}</p>
+                <p>{detailsClientNeed?.description}</p>
               </div>
             </div>
         </div>
