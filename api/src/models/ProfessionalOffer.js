a0656dee8945ea11395df090f8b1568af57c7926
Guillermo Villagomez <gvdelamora@gmail.com>
2021-12-31 09:23:32 -0600

    get offers by user id done

--- api/src/models/ProfessionalOffer.js
+++ api/src/models/ProfessionalOffer.js
@@ -2,25 +2,29 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     return sequelize.define('ProfessionalOffer', {
-        description: {
-            type: DataTypes.STRING,
-            // allowNull: false,
-        },
-        price: {
-            type: DataTypes.INTEGER,
-            // allowNull: false,
-        },
-        duration: {
-            type: DataTypes.INTEGER,
-            // allowNull: false,
-        },
-        materials: {
-            type: DataTypes.BOOLEAN,
-            // allowNull: false,
-        },
-        guarantee_time: {
-            type: DataTypes.INTEGER,
-            // allowNull: false,
-        },
+      name: {
+        type: DataTypes.STRING,
+        // allowNull: false,
+      },
+      description: {
+          type: DataTypes.STRING,
+          // allowNull: false,
+      },
+      price: {
+          type: DataTypes.INTEGER,
+          // allowNull: false,
+      },
+      duration: {
+          type: DataTypes.INTEGER,
+          // allowNull: false,
+      },
+      materials: {
+          type: DataTypes.BOOLEAN,
+          // allowNull: false,
+      },
+      guarantee_time: {
+          type: DataTypes.INTEGER,
+          // allowNull: false,
+      },
     }, {});
 }
