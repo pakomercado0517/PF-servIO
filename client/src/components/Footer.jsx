dc78839b65f87aa5e87ace9c2311893de64c1de0
anamad8 <stellam_anabel@hotmail.com>
2022-01-04 17:29:14 -0300

    footer listo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import s from './styles/Footer.module.css';
 import img from '../img/ServOIOFooter.svg';
 import {Link, NavLink} from 'react-router-dom';
+import linkedin from '../img/linkedin.svg';
+import github from '../img/github.svg'
 
 export default function footer() {
     return (
@@ -14,99 +16,64 @@ export default function footer() {
             </div>
             <div className={s.contacto}>
                 <div className={s.contacto_integrante}>
-                    <svg 
-                        stroke="currentColor" 
-                        fill="currentColor" 
-                        strokeWidth="0" 
-                        viewBox="0 0 24 24" 
-                        className="NavBar_iconLogin__3M-LE" 
-                        height="1em" 
-                        width="1em" 
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path fill="none" d="M0 0h24v24H0z"></path>
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2a7.2 7.2 0 01-6-3.22c.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08a7.2 7.2 0 01-6 3.22z"></path>
-                    </svg>
-                    <a href="https://www.linkedin.com/in/imanolcorimayo">
+                    <div className={s.contacto_integrante_img}>
+                        <a href="https://www.linkedin.com/in/imanolcorimayo" target="_blank">
+                            <img src={linkedin} alt='img'></img>
+                        </a>
+                        <a href="https://github.com/imanolcorimayo"  target="_blank">
+                        <img src={github} alt='img'></img>
+                    </a>
+                    </div>
                     <p>Imanol Corimayo</p>
-                    </a>
                 </div>
                 <div className={s.contacto_integrante}>
-                    <svg 
-                        stroke="currentColor" 
-                        fill="currentColor" 
-                        strokeWidth="0" 
-                        viewBox="0 0 24 24" 
-                        className="NavBar_iconLogin__3M-LE" 
-                        height="1em" 
-                        width="1em" 
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path fill="none" d="M0 0h24v24H0z"></path>
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2a7.2 7.2 0 01-6-3.22c.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08a7.2 7.2 0 01-6 3.22z"></path>
-                    </svg>
-                    <a href="https://www.linkedin.com/in/anabel-amad-/">
+                <div className={s.contacto_integrante_img}>
+                    <a href="https://www.linkedin.com/in/anabel-amad-/" target="_blank">
+                        <img src={linkedin} alt='img'></img>
+                    </a>
+                    <a href="https://github.com/anamad8" target="_blank">
+                        <img src={github} alt='img'></img>
+                    </a>
+                </div>
                     <p>Anabel Amad</p>
-                    </a>
                 </div>
                 <div className={s.contacto_integrante}>
-                    <svg 
-                        stroke="currentColor" 
-                        fill="currentColor" 
-                        strokeWidth="0" 
-                        viewBox="0 0 24 24" 
-                        className="NavBar_iconLogin__3M-LE" 
-                        height="1em" 
-                        width="1em" 
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path fill="none" d="M0 0h24v24H0z"></path>
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2a7.2 7.2 0 01-6-3.22c.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08a7.2 7.2 0 01-6 3.22z"></path>
-                    </svg>
-                    <a href="https://www.linkedin.com/in/francisco-mercado-escalante/">
+                <div className={s.contacto_integrante_img}>
+                    <a href="https://www.linkedin.com/in/francisco-mercado-escalante/" target="_blank">
+                        <img src={linkedin} alt='img'></img>
+                    </a>
+                    <a href="https://github.com/pakomercado0517"  target="_blank">
+                        <img src={github} alt='img'></img>
+                    </a>
+                </div>
                     <p>Francisco Mercado Escalante</p>
-                    </a>
                 </div>
                 <div className={s.contacto_integrante}>
-                    <svg 
-                        stroke="currentColor" 
-                        fill="currentColor" 
-                        strokeWidth="0" 
-                        viewBox="0 0 24 24" 
-                        className="NavBar_iconLogin__3M-LE" 
-                        height="1em" 
-                        width="1em" 
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path fill="none" d="M0 0h24v24H0z"></path>
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2a7.2 7.2 0 01-6-3.22c.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08a7.2 7.2 0 01-6 3.22z"></path>
-                    </svg>
-                    <a href="https://www.linkedin.com/in/guillermo-de-la-mora/">
+                <div className={s.contacto_integrante_img}>
+                    <a href="https://www.linkedin.com/in/guillermo-de-la-mora/" target="_blank">
+                        <img src={linkedin} alt='img'></img>
+                    </a>
+                    <a href=""  target="_blank">
+                        <img src={github} alt='img'></img>
+                    </a>
+                </div>
                     <p>Guillermo De la Mora</p>
-                    </a>
                 </div>
                 <div className={s.contacto_integrante}>
-                    <svg 
-                        stroke="currentColor" 
-                        fill="currentColor" 
-                        strokeWidth="0" 
-                        viewBox="0 0 24 24" 
-                        className="NavBar_iconLogin__3M-LE" 
-                        height="1em" 
-                        width="1em" 
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path fill="none" d="M0 0h24v24H0z"></path>
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2a7.2 7.2 0 01-6-3.22c.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08a7.2 7.2 0 01-6 3.22z"></path>
-                    </svg>
-                    <a href="http://www.linkedin.com/in/fernando-miguel-de-olazabal">
-                        <p>Fernando de Olazábal</p>
-                    </a>
+                    <div className={s.contacto_integrante_img}>
+                        <a href="http://www.linkedin.com/in/fernando-miguel-de-olazabal" target="_blank">
+                            <img src={linkedin} alt='img'></img>
+                        </a>
+                        <a href="https://github.com/Ferdeolazabal"  target="_blank">
+                            <img src={github} alt='img'></img>
+                        </a>
+                    </div>
+                    <p>Fernando de Olazábal</p>
                 </div>
                 
             </div>
             <div className={s.fin}>
-                <p >Copyright of ServIO 2021</p>
+                <p >Copyright of ServIO 2022</p>
             </div>
             
         </div>
