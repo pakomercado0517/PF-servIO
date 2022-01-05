38b99a00df1f8f4b72b8af5d666a2ae67566485d
imanol <imanolcorimayo@gmail.com>
2022-01-05 08:58:11 -0300

    add update on professionalOffers!

--- api/src/routes/ProfessionalOffers.js
+++ api/src/routes/ProfessionalOffers.js
@@ -2,10 +2,16 @@ const { Router } = require("express");
 const router = Router();
 const offerFunctions = require('../controllers/index.js')
 
-router.post('/', offerFunctions.newProfessionalOffer)
+// GET METHODS
 router.get('/all/:id', offerFunctions.getOffersByUserId)
 router.get('/all', offerFunctions.getAllPorfessionalOffers)
 router.get('/:id', offerFunctions.getUserReceivedOffers )
 router.get('/need/:id', offerFunctions.getNeedReceivedOffers )
-router.delete('/:id', offerFunctions.deleteOfferById )
+// POST METHODS
+router.post('/', offerFunctions.newProfessionalOffer)
+// DELETE METHODS
+router.delete('/:id', offerFunctions.deleteOfferById)
+// PUT METHODS
+router.put('/:id', offerFunctions.updateOffer )
+
 module.exports= router;
