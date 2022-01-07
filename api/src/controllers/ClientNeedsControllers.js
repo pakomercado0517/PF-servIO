9e293ed3eb66059eeee42e8e82b52c0d9a415bdf
Guillermo Villagomez <gvdelamora@gmail.com>
2022-01-07 15:45:50 -0600

    Client Need controller update

---
+++ api/src/controllers/ClientNeedsControllers.js
@@ -0,0 +1,173 @@
+const Sequelize = require("sequelize");
+const {
+  User,
+  Profession,
+  Professional,
+  ProfessionalOffer,
+  ClientNeed,
+  SpecificTechnicalActivity,
+  Transactions,
+  Profession_Professional,
+  ClientReview,
+} = require("../db.js");
+
+module.exports = {
+  getAllNeeds: async (req, res) => {
+    try {
+      const needs = await ClientNeed.findAll({
+        include: [{ model: User }, { model: ProfessionalOffer }],
+      });
+      res.status(200).send(needs);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  },
+
+  getNeedByName: async (req, res) => {
+    try {
+      const need = await ClientNeed.findAll({
+        include: [{ model: User }],
+        where: { name: { [Sequelize.Op.iLike]: `%${req.query.name}%` } },
+      });
+      res.status(200).send(need);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  },
+
+  getNeedsByUserId: async (req, res) => {
+    const id = req.params.id;
+    try {
+      if (id > 0) {
+        const needs = await ClientNeed.findAll({
+          where: { UserId: id },
+        });
+        if (needs) {
+          res.status(200).send(needs);
+        } else {
+          res.status(200).send("User does not have any need");
+        }
+      } else {
+        res.status(200).send("Please insert an id");
+      }
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  },
+
+  updateNeed: async (req, res) => {
+    const {
+      name,
+      description,
+      location,
+      status, //   price,//   duration,//   guarantee_time
+    } = req.body;
+    const id = req.params.id;
+    try {
+      const need = await ClientNeed.findOne({
+        where: { id },
+      });
+
+      if (need) {
+        need.name = name ? name : need.name;
+        need.description = description ? description : need.description;
+        need.location = location ? location : need.location;
+        if (
+          status === "done" ||
+          status === "in progress" ||
+          status === "in offer"
+        ) {
+          need.status = status;
+        } else {
+          need.status = need.status;
+        }
+
+        await need.save();
+
+        res.status(200).send(need);
+      } else {
+        res.status(400).send("Inserta Id de necesidad existente");
+      }
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  },
+
+  newSpecificalNeed: async (req, res) => {
+    const {
+      name,
+      description,
+      location,
+      photo,
+      //   price,
+      //   duration,
+      //   guarantee_time,
+      userId,
+    } = req.body;
+    try {
+      if (userId) {
+        const newNeed = await ClientNeed.create({
+          name,
+          description,
+          status: "in offer",
+          location,
+          photo
+          //   price,
+          //   duration,
+          //   guarantee_time,
+        });
+
+        let allUsers = await User.findAll({
+          where: {
+            id: parseInt(userId),
+          },
+        });
+
+        await newNeed.setUser(allUsers[0]);
+        let userWithNeed = await User.findAll({
+          where: {
+            id: parseInt(userId),
+          },
+          include: [{ model: ClientNeed }],
+        });
+        res.status(200).send(newNeed);
+      } else {
+        res.status(400).send("Please login");
+      }
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  },
+
+  getById: async (req, res) => {
+    const id = req.params.id;
+    if (id) {
+      const need = await ClientNeed.findOne({
+        where: {
+          id,
+        },
+      });
+      res.status(200).send(need);
+    }
+    else {
+      res.status(400).send("Please insert an id");
+    }
+  },
+  
+  deleteNeedById: async (req, res) => {
+    const id = req.params.id;
+    if(id){
+      const need = await ClientNeed.findOne({ where: { id } });
+      if (need){
+        need.destroy();
+        res.status(200).send( "La necesidad especifica ha sido eliminada.");
+      } else {
+        res.status(404).send("Need not found")
+      }
+    }else{
+      res.status(200).send( "Por favor inserta un id.");
+    }
+    
+    
+  },
+};
--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -6,9 +6,7 @@ const passport = require("passport");
 const enviarEmail = require("../handlers/email");
 const crypto = require("crypto");
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
-var juice = require("juice");
+// var juice = require("juice");
 // @ts-ignore
 const {
   User,
@@ -689,60 +687,8 @@ module.exports = {
   },
 
   // ************ CLIENT NEEDS
-  
-  getAllNeeds: async (req, res) => {
-    try {
-      const needs = await ClientNeed.findAll({
-        include: [{ model: User }, { model: ProfessionalOffer }],
-      });
-      res.status(200).send(needs);
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
-  },
 
-  getNeedByName: async (req, res) => {
-    try {
-      const need = await ClientNeed.findAll({
-        include: [{ model: User }],
-        where: { name: { [Sequelize.Op.iLike]: `%${req.query.name}%` } },
-      });
-      res.status(200).send(need);
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
-  },
 
-  getNeedsById: async (req, res) => {
-    const id = req.params.id;
-    try {
-      if (id > 0) {
-        const needs = await ClientNeed.findAll({
-          where: { UserId: id },
-        });
-        if (needs) {
-          res.status(200).send(needs);
-        } else {
-          res.status(200).send("User does not have any need");
-        }
-      } else {
-        res.status(200).send("Please insert an id");
-      }
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
-  },
-
-  // newSpecificalNeed: async (req, res) =>{
-  //     const {name, description, location} = req.body
-  //     const newNeed = await ClientNeed.create({
-  //         name,
-  //         description,
-  //         location,
-  //         status: 'in offer'
-  //     })
-  //     res.send(newNeed)
-  // },
   // getByProfessionName: async (req, res) =>{
   //     const {profession} = req.body
   //     const professionalArr = profession.split(',')
@@ -752,119 +698,6 @@ module.exports = {
   //                 model: Profession
   //             }],
   //         })
-
-  getById: async (req, res) => {
-    const id = req.params.id;
-    if (id) {
-      const need = await ClientNeed.findOne({
-        where: {
-          id,
-        },
-      });
-      res.status(200).send(need);
-    }
-     else {
-      res.status(400).send("Please insert an id");
-    }
-  },
-
-  newSpecificalNeed: async (req, res) => {
-    const {
-      name,
-      description,
-      location,
-      photo,
-      //   price,
-      //   duration,
-      //   guarantee_time,
-      userId,
-    } = req.body;
-    try {
-      if (userId) {
-        const newNeed = await ClientNeed.create({
-          name,
-          description,
-          status: "in offer",
-          location,
-          photo
-          //   price,
-          //   duration,
-          //   guarantee_time,
-        });
-
-        let allUsers = await User.findAll({
-          where: {
-            id: parseInt(userId),
-          },
-        });
-
-        await newNeed.setUser(allUsers[0]);
-        let userWithNeed = await User.findAll({
-          where: {
-            id: parseInt(userId),
-          },
-          include: [{ model: ClientNeed }],
-        });
-        res.status(200).send(newNeed);
-      } else {
-        res.status(400).send("Please login");
-      }
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
-  },
-
-  updateNeed: async (req, res) => {
-    const {
-      name,
-      description,
-      location,
-      status, //   price,//   duration,//   guarantee_time
-    } = req.body;
-    const id = req.params.id;
-    try {
-      const need = await ClientNeed.findOne({
-        where: { id },
-      });
-
-      if (need) {
-        need.name = name ? name : need.name;
-        need.description = description ? description : need.description;
-        need.location = location ? location : need.location;
-        if (
-          status === "done" ||
-          status === "in progress" ||
-          status === "in offer"
-        ) {
-          need.status = status;
-        } else {
-          need.status = need.status;
-        }
-
-        await need.save();
-
-        res.status(200).send(need);
-      } else {
-        res.status(400).send("Inserta Id de necesidad existente");
-      }
-    } catch (error) {
-      res.status(400).send(error.message);
-    }
-  },
-
-  deleteNeedById: async (req, res) => {
-    const id = req.params.id;
-    const need = await ClientNeed.findOne({ where: { id } });
-    if (need.id){
-      need.destroy();
-      res.send(
-        "La necesidad especifica ha sido eliminada."
-      );
-    } else {
-      res.status(404).send("Need not found")
-    }
-    
-  },
 
   // ************ PROFESSIONAL OFFERS
   
