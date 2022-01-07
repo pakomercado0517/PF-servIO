05f8df17b4763c3260083f8a82483c12242c3c8f
Guillermo Villagomez <gvdelamora@gmail.com>
2022-01-06 19:32:01 -0600

    routes restricted donr

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import { useGlobalStorage } from './hooks/useGlobalStorage';
 // Router-dom
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import ProfessionalSpecificActivity from "./pages/ProfessionalSpecificActivity";
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
@@ -24,50 +25,51 @@ import ResetPassword from './pages/ResetPassword';
 import ActivateAccount from './pages/ActivateAccount';
 
 function App() {
+  const [globalUser,setGlobalUser ] = useGlobalStorage("globalUser", "")
   return (
     <>
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/nosotros" element={<Nosotros />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/activate/:token" element={<ActivateAccount />} />
-        <Route exact path="/forget-password" element={<ForgetPassword />} />
-        <Route exact path="/forget-password/:token" element={<ResetPassword />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/clients/:idClient" element={<ProfileClient />} />
-        <Route exact path="/professional/:id" element={<ProfileProfessional />} />
+        <Route exact path="/login" element={globalUser !=='' ? <Navigate to='/'/> : <Login />}/>
+        <Route exact path="/activate/:token" element={globalUser !=='' ? <Navigate to='/'/> :<ActivateAccount />} />
+        <Route exact path="/forget-password" element={globalUser !=='' ? <Navigate to='/'/> :<ForgetPassword />} />
+        <Route exact path="/forget-password/:token" element={globalUser !=='' ? <Navigate to='/'/> :<ResetPassword />} />
+        <Route exact path="/register" element={globalUser !=='' ? <Navigate to='/'/> : <Register />} />
+        <Route exact path="/clients/:idClient" element={globalUser ==='' ? <Navigate to='/login'/> :<ProfileClient />} />
+        <Route exact path="/professional/:id" element={globalUser ==='' ? <Navigate to='/login'/> :<ProfileProfessional />} />
         <Route exact path="/cart/" element={<Cart />} />
-        <Route exact path="/homeprofessional" element={<HomeProfessional />} />
-        <Route exact path="/editUser" element={<EditUser />} />
-        <Route exact path="/service-history/:id" element={<ServiceHistory />} />
+        {/* <Route exact path="/homeprofessional" element={<HomeProfessional />} /> */}
+        <Route exact path="/editUser" element={globalUser ==='' ? <Navigate to='/login'/> :<EditUser />} />
+        <Route exact path="/service-history/:id" element={globalUser ==='' ? <Navigate to='/login'/> : <ServiceHistory />} />
         <Route
           exact path="/ProfessionalServiceOffer"
-          element={<ProfessionalServiceOffer />}
+          element={globalUser === '' ? <Navigate to='/login'/> :<ProfessionalServiceOffer />}
         />
         <Route
           exact path="/ProfessionalOfferToClientNeed/:idClientNeed"
-          element={<ProfessionalOfferToClientNeed />}
+          element={globalUser ==='' ? <Navigate to='/login'/> :<ProfessionalOfferToClientNeed />}
         />
         <Route
           exact path="/professional/:idProfessional/SpecificActivity/:id"
-          element={<ProfessionalSpecificActivity />}
+          element={globalUser ==='' ? <Navigate to='/login'/> :<ProfessionalSpecificActivity />}
         />
         <Route
           exact path="/professional/activity/edit/:id"
-          element={<ProfessionalSpecificActivityEdit />}
+          element={globalUser ==='' ? <Navigate to='/login'/> :<ProfessionalSpecificActivityEdit />}
         />
         <Route
           exact path="/client/need/:id"
-          element={<DetailsClientNeed />}
+          element={globalUser ==='' ? <Navigate to='/login'/> :<DetailsClientNeed />}
         />
         <Route
           exact path="/client/offerToNeed/:id"
-          element={<OffersToSpecificClientsNeeds />}
+          element={globalUser ==='' ? <Navigate to='/login'/> :<OffersToSpecificClientsNeeds />}
         />
         <Route
           exact path="/client/:userId/edit/:publicationId"
-          element={<ClientSpecificNeedEdit />}
+          element={globalUser ==='' ? <Navigate to='/login'/> :<ClientSpecificNeedEdit />}
         />
         
       </Routes>
